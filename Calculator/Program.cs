1f5ee825248041838cc7c09d03e1e611e1f283b4
nitemi@ <blessedtgirl@gmail.com>
2024-01-30 12:58:05 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+static void Main(string[] args)
+{
+
+    int value1;
+    int value2;
+
+    Console.WriteLine("input value1 and press enter");
+
+    value1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("input value 2 and press enter");
+
+    value2 = Convert.ToInt32(Console.ReadLine());
+
+    int result = (value1 + value2);
+
+    Console.WriteLine("result");
+}
